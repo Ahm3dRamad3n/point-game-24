1bdf00f97bbcd839c85e996f48ffa43e5cbbc937
Ahmed Ramadan Abd Elrady <ahmd.abdalrada2456@compit.aun.edu.eg>
2026-04-21 21:10:13 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,17 @@
 const CACHE_NAME = 'pwa-cache-v1';
 const urlsToCache = [
-  '',
-  'index.html',
-  'manifest.json',
-  'icons/icon-48x48.png',
-  'icons/icon-72x72.png',
-  'icons/icon-96x96.png',
-  'icons/icon-128x128.png',
-  'icons/icon-144x144.png',
-  'icons/icon-152x152.png',
-  'icons/icon-192x192.png',
-  'icons/icon-384x384.png',
-  'icons/icon-512x512.png',
-  'game-logic.js',
-  'styles.css',
-
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-48x48.png',
+  './icons/icon-72x72.png',
+  './icons/icon-96x96.png',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
+  './icons/icon-192x192.png',
+  './icons/icon-384x384.png',
+  './icons/icon-512x512.png',
 ];
 
 self.addEventListener('install', event => {
